9e3cc002c547bae0ed02a2744e4f71cd1102e9f5
Julia W <w.julia91@gmail.com>
2020-04-16 16:45:03 +0200

    dice removed

--- app.js
+++ app.js
@@ -20,3 +20,7 @@ dice = Math.floor(Math.random() * 6) + 1; //Math.floor() removes the decimal of 
 
 document.querySelector('#current-' + activePlayer).textContent = dice;
 // document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+var x = document.querySelector('#score-0').textContent;
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none';
