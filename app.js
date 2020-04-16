3c08a27cd3703f07a212e13a76b8637847f48338
Julia W <w.julia91@gmail.com>
2020-04-16 12:04:56 +0200

    1st DOM manipulation

--- app.js
+++ app.js
@@ -16,4 +16,6 @@ roundScore = 0;
 activePlayer = 0; //This will be used later to index in the scores array.
 
 dice = Math.floor(Math.random() * 6) + 1; //Math.floor() removes the decimal of a number. Times 6 plus 1 will give us number BETWEEN 1 and 6. Math.ranom () gives a random number between 0 and 1.
-console.log(dice);
+// console.log(dice);
+
+document.querySelector('#current-0').textContent = dice;
