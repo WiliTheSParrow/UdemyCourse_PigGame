7d9e725605c39589c8356383497482a947954ba3
Julia W <w.julia91@gmail.com>
2020-04-20 14:42:57 +0200

    nextPlayer function added

--- app.js
+++ app.js
@@ -63,10 +63,26 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
     // Update the UI
     document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
+    // Next player
 
 
 });
 
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+
+    document.getElementById('current-0').textContent = '0';
+    document.getElementById('current-1').textContent = '0';
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    /* document.querySelector('.player-0-panel').classList.remove('active');
+    document.querySelector('.player-1-panel').classList.add('active'); */
+
+    document.querySelector('.dice').style.display = 'none';
+}
 
 
 
